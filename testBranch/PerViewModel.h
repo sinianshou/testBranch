cb791c478b68dbacd38a95243646a5f4908d1762
Liu.Easer <253091451@qq.com>
2018-02-01 09:38:05 +0800

    none

---
+++ testBranch/PerViewModel.h
@@ -0,0 +1,13 @@
+//
+//  PerViewModel.h
+//  testBranch
+//
+//  Created by Easer on 2018/1/31.
+//  Copyright © 2018年 Easer. All rights reserved.
+//
+
+#import <ReactiveViewModel/ReactiveViewModel.h>
+
+@interface PerViewModel : RVMViewModel
+
+@end
---
+++ testBranch/PerViewModel.m
@@ -0,0 +1,13 @@
+//
+//  PerViewModel.m
+//  testBranch
+//
+//  Created by Easer on 2018/1/31.
+//  Copyright © 2018年 Easer. All rights reserved.
+//
+
+#import "PerViewModel.h"
+
+@implementation PerViewModel
+
+@end
